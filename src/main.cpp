acfed92ed3a05c57944fbf61d25d55280ed3aa85
Petteri Suonpää <petteri@Petteri>
2024-11-22 12:58:53 +0200

    added so comment lines

--- src/main.cpp
+++ src/main.cpp
@@ -7,51 +7,65 @@ using namespace sf;
 
 int main()
 {
-    RenderWindow app(VideoMode(1024, 768), "Car Racing Game!");
+    float windowHeight = 1024;
+    float windowWidth = 768;
+
+    RenderWindow app(VideoMode(windowHeight, windowWidth), "Car Racing Game!");
+    //ensures the game runs at 60 frames per second
     app.setFramerateLimit(60);
 
     Texture t1,t2,t3;
+    //load the map images
     t1.loadFromFile("images/background.png");
     t2.loadFromFile("images/car.png");
     t1.setSmooth(true);
     t2.setSmooth(true);
     
-    // play music
+    // play music 
     SoundManager::playMusic("sounds/Hotline Miami Soundtrack ~ Hydrogen [TubeRipper.cc].ogg");
 
-
-
     Sprite sBackground(t1), sCar(t2);
+    //scales the background sprite to twice its original size.
     sBackground.scale(2,2);
-
+    // sets the origin
     sCar.setOrigin(22, 22);
     float R=22;
 
+    // number of cars in the game
     const int N=5;
     Car car[N];
+    //initializes postion and speed for each car
     for(int i=0;i<N;i++)
     {
+      // x and y coordinate of each car
       car[i].x=300+i*50;
       car[i].y=1700+i*80;
+      // increase speed for each car
       car[i].speed=7+i;
     }
 
+    // Car Movement Parameters
    float speed=0,angle=0;
    float maxSpeed=12.0;
    float acc=0.2, dec=0.3;
    float turnSpeed=0.08;
 
+    //  These are used to adjust the background based on the car's position, effectively creating a scrolling effect as the car moves.
    int offsetX=0,offsetY=0;
 
+    // run the program as long as the window is open
     while (app.isOpen())
     {
+        // check all the window's events that were triggered since the last iteration of the loop
         Event e;
         while (app.pollEvent(e))
         {
+             // "close requested" event: we close the window
             if (e.type == Event::Closed)
                 app.close();
         }
 
+    // Handling User Input
     bool Up=0,Right=0,Down=0,Left=0;
     if (Keyboard::isKeyPressed(Keyboard::Up)) Up=1;
     if (Keyboard::isKeyPressed(Keyboard::Right)) Right=1;
@@ -71,15 +85,16 @@ int main()
         if (speed - dec > 0) speed -= dec;
         else if (speed + dec < 0) speed += dec;
         else speed = 0;
-
+    
+    //Turning the Car
     if (Right && speed!=0)  angle += turnSpeed * speed/maxSpeed;
     if (Left && speed!=0)   angle -= turnSpeed * speed/maxSpeed;
 
     car[0].speed = speed;
     car[0].angle = angle;
-
+    // update positions (move) and find the next position (findtarget)
     for(int i=0;i<N;i++) car[i].move();
-    for(int i=1;i<N;i++) car[i].findTarget();
+    //for(int i=1;i<N;i++) car[i].findTarget();
 
     //collision
     for(int i=0;i<N;i++)
@@ -98,7 +113,7 @@ int main()
          }
     }
 
-
+    //Background and Car Rendering
     app.clear(Color::White);
 
     if (car[0].x>320) offsetX = car[0].x-320;
@@ -106,7 +121,8 @@ int main()
 
     sBackground.setPosition(-offsetX,-offsetY);
     app.draw(sBackground);
-
+    
+    // Drawing Cars
     Color colors[10] = {Color::Red, Color::Green, Color::Magenta, Color::Blue, Color::White};
 
     for(int i=0;i<N;i++)
@@ -116,7 +132,7 @@ int main()
       sCar.setColor(colors[i]);
       app.draw(sCar);
     }
-
+    //
     app.display();
     }
 
